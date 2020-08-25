5e5de87a10792a46dd5e3e7d95297b746b84773d
George Fouche <fouliex@gmail.com>
2020-08-25 15:30:45 -0700

    Add Notification Email Entity

--- src/main/java/com/georgeerol/CloneReddit/model/NotificationEmail.java
+++ src/main/java/com/georgeerol/CloneReddit/model/NotificationEmail.java
@@ -1,7 +1,20 @@
 package com.georgeerol.CloneReddit.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * Created by George Fouche on 8/25/20.
  */
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NotificationEmail {
+    private String subject;
+    private String recipient;
+    private String body;
+
 }
