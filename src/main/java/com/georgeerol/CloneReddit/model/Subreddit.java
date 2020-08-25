4549066bc599a4239ea05aa3d0eb33dd730d5dad
George Fouche <fouliex@gmail.com>
2020-08-25 16:36:55 -0700

    Add Subreddit Entity

--- src/main/java/com/georgeerol/CloneReddit/model/Subreddit.java
+++ src/main/java/com/georgeerol/CloneReddit/model/Subreddit.java
@@ -3,5 +3,36 @@ package com.georgeerol.CloneReddit.model;
 /**
  * Created by George Fouche on 8/25/20.
  */
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.time.Instant;
+import java.util.List;
+
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
 public class Subreddit {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    @NotBlank(message = "Cummunity name is required")
+    private String name;
+    @NotBlank(message = "Description is required")
+    private String description;
+    @OneToMany(fetch = LAZY)
+    private List<Post> posts;
+    private Instant createdDate;
+    @ManyToOne(fetch = LAZY)
+    private User user;
 }
