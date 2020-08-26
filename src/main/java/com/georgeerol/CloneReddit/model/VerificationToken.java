0907e413144f8cd2246af088eca5769bd0a2e91e
George Fouche <fouliex@gmail.com>
2020-08-25 17:11:14 -0700

    Add Verification Token Entity

--- src/main/java/com/georgeerol/CloneReddit/model/VerificationToken.java
+++ src/main/java/com/georgeerol/CloneReddit/model/VerificationToken.java
@@ -1,7 +1,29 @@
 package com.georgeerol.CloneReddit.model;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
 /**
  * Created by George Fouche on 8/25/20.
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "token")
 public class VerificationToken {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    private String token;
+    @OneToOne(fetch = LAZY)
+    private User user;
+    private Instant expiryDate;
 }
