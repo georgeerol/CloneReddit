668aef367cb22814cff9398e57ec207df7229998
George Fouche <fouliex@gmail.com>
2020-08-25 17:38:49 -0700

    Add Vote Entity

--- src/main/java/com/georgeerol/CloneReddit/model/Vote.java
+++ src/main/java/com/georgeerol/CloneReddit/model/Vote.java
@@ -1,7 +1,34 @@
 package com.georgeerol.CloneReddit.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
+
 /**
  * Created by George Fouche on 8/25/20.
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
 public class Vote {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long voteId;
+    private VoteType voteType;
+    @NotNull
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "postId", referencedColumnName = "postId")
+    private Post post;
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private User user;
 }
